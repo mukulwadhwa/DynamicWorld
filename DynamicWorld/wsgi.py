c08ec4b6c83635047b4fb965d6d0fb35d896b6b9
Mukul Wadhwa <mukulwadhwa99@gmail.com>
2023-06-24 15:40:18 +0530

    Initial Commit

---
+++ DynamicWorld/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for DynamicWorld project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DynamicWorld.settings')
+
+application = get_wsgi_application()
